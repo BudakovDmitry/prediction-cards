631356c0169555908f8abc5eed81af4f71fa126a
Budakov Dmitry <budakov.it@gmail.com>
2022-09-06 23:08:13 +0300

    Add styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,6 @@
 import { createRoot } from 'react-dom/client';
 
+import './index.css'
+
 const root = createRoot(document.getElementById('root') as HTMLElement);
 root.render(<h1>Yeeeeees!</h1>);
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   mode: 'development',
@@ -10,9 +11,27 @@ module.exports = {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: './'
+            }
+          },
+          'css-loader'
+        ]
       }
     ]
   },
+  plugins: [
+    new HtmlWebpackPlugin({ template: './index.html' }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
+    })
+  ],
   resolve: {
     extensions: ['.tsx', '.ts', '.js']
   },
@@ -20,7 +39,6 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js'
   },
-  plugins: [new HtmlWebpackPlugin({ template: './index.html' })],
   devServer: {
     static: {
       directory: path.join(__dirname, 'public')
