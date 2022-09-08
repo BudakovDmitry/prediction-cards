0d59a8734e8f79abfc6081b09e604a98524f393a
Budakov Dmitry <budakov.it@gmail.com>
2022-09-08 21:21:49 +0300

    Done Right field

--- src/components/RightField/index.tsx
+++ src/components/RightField/index.tsx
@@ -1,15 +1,22 @@
+import { useRightField } from 'src/components/RightField/useRightField'
 import * as Styled from 'src/components/RightField/styles'
+import Loader from 'src/components/Loader'
+import Card from 'src/components/Card'
 
 const RightField = () => {
+  const { randomNumber, getRandomNumber, loading, handleViewCard, card, viewCard } = useRightField()
+
   return (
     <Styled.RightFieldContainer>
-      <Styled.HeaderText>
+      {viewCard ? <Card id={card.id} title={card.title} description={card.description} handleViewCard={handleViewCard} /> : (<><Styled.HeaderText>
         Доверь свою судьбу случайности!
       </Styled.HeaderText>
       <Styled.NumberContainer>
-        <Styled.Number>7</Styled.Number>
+        {loading ? <Loader color="#ffffff" /> : <Styled.Number>{randomNumber}</Styled.Number>}
       </Styled.NumberContainer>
-      <Styled.Button>Нажимай</Styled.Button>
+      {randomNumber ? 
+      <Styled.Button onClick={handleViewCard}>Открыть карту</Styled.Button> : 
+      <Styled.Button onClick={getRandomNumber}>Нажимай</Styled.Button>}</>)}
     </Styled.RightFieldContainer>
   )
 }
---
+++ src/components/RightField/useRightField.ts
@@ -0,0 +1,44 @@
+import { useState } from "react"
+import json from 'src/json/cards.json'
+
+export const useRightField = () => {
+  const [randomNumber, setRandomNumber] = useState<number | null>(null)
+  const [loading, setLoading] = useState<boolean>(false)
+  const [viewCard, setViewCard] = useState<boolean>(false)
+  const [ card, setCard ] = useState({
+    id: 0,
+    title: '',
+    description: ''
+  })
+
+  const getRandomNumber = () => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+      setRandomNumber(Math.floor(Math.random() * (45 - 1)) + 1)
+    }, 1500)
+  }
+
+  const handleViewCard = (): void => {
+    viewCard ? hideCard() : randomNumber && openCard(randomNumber)
+  }
+
+  const hideCard = () => {
+    setViewCard(false)
+    setRandomNumber(null)
+  }
+
+  const openCard = (id: number) => {
+    setCard({...json.cards[id - 1]})
+    setViewCard(true)
+  }
+
+  return {
+    randomNumber,
+    getRandomNumber,
+    loading,
+    card,
+    handleViewCard,
+    viewCard
+  }
+}
