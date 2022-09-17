888faebd25a37de8606e9992a669de7261787bca
Budakov Dmitry <budakov.it@gmail.com>
2022-09-18 00:07:25 +0300

    Update Welcome modal

--- src/components/WelcomeModal/index.tsx
+++ src/components/WelcomeModal/index.tsx
@@ -19,17 +19,17 @@ const WelcomeModal = ({ setActive = () => {} }: WelcomeModalProps) => {
       <Styled.WelcomeModalDescription>
         Хочу познайомити тебе з інструментом, який допоможе тобі знайти
         відповідь або підказку на питання, що хвилює тебе.
-        <Styled.Instruction>
-          <Styled.InstructionItem>
-            По-перше, тобі потрібно подумати про те, що наразі важливо для тебе!
-          </Styled.InstructionItem>
-          <Styled.InstructionItem>
-            Потім, тобі треба вирішити обирати долю самому, або довірити долю
-            випадковості, та обрати карту!
-          </Styled.InstructionItem>
-          <Styled.InstructionItem>Довіритись!</Styled.InstructionItem>
-        </Styled.Instruction>
       </Styled.WelcomeModalDescription>
+      <Styled.Instruction>
+        <Styled.InstructionItem>
+          По-перше, тобі потрібно подумати про те, що наразі важливо для тебе!
+        </Styled.InstructionItem>
+        <Styled.InstructionItem>
+          Потім, тобі треба вирішити обирати долю самому, або довірити долю
+          випадковості, та обрати карту!
+        </Styled.InstructionItem>
+        <Styled.InstructionItem>Довіритись!</Styled.InstructionItem>
+      </Styled.Instruction>
 
       <a className="welcome-text__link" onClick={() => setActive(false)}>
         <span>Поїхали!</span>
