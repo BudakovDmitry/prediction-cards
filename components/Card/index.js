fccb7351f60b5a8b63d56b2668413e9ffe944b39
Budakov Dmitry <budakov.it@gmail.com>
2022-09-18 00:08:05 +0300

    Updates

--- components/Card/index.js
+++
@@ -1,6 +0,0 @@
-import { jsxs as _jsxs, jsx as _jsx, Fragment as _Fragment } from "react/jsx-runtime";
-import * as Styled from 'src/components/Card/styles';
-const Card = ({ id = 0, title = '', description = '', setActiveModal = () => { }, }) => {
-    return (_jsxs(_Fragment, { children: [_jsxs(Styled.Card, { children: [_jsxs(Styled.CardNumber, { children: ["\u269B ", id, " \u269B"] }), _jsx(Styled.CardTitle, { children: title }), _jsx(Styled.CardDescription, { children: description })] }), _jsx(Styled.Button, { onClick: setActiveModal, children: "\u0417\u0430\u043A\u0440\u0438\u0442\u0438" })] }));
-};
-export default Card;
--- components/Modal/index.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import * as Styled from 'src/components/Modal/styles';
-const Modal = ({ active = false, setActive = () => { }, children = null, }) => {
-    return (_jsx(Styled.ModalContainer, { active: active, onClick: () => setActive(false), children: _jsx(Styled.ModalContent, { active: active, onClick: (event) => event.stopPropagation(), children: children }) }));
-};
-export default Modal;
